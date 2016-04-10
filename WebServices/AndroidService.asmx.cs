32a72f49a4f1a52108e386ef7d07577f617b306c
ozany <ozany@la.com.tr>
2016-04-10 12:25:16 +0300

    İlan Insert/Update metodu yazıldı -> DoInsertAndUpdateAdvert

--- WebServices/AndroidService.asmx.cs
+++ WebServices/AndroidService.asmx.cs
@@ -56,7 +56,7 @@ namespace WebServices
         }
 
         [WebMethod]
-        public Result<bool> SaveUser(bool IsNewUser_, string Username_, string NameSurname_, string Email_, string Phone_, string Password_)
+        public Result<bool> DoInsertAndUpdateUser(bool IsNewUser_, string Username_, string NameSurname_, string Email_, string Phone_, string Password_)
         {
             Result<bool> result_ = new Result<bool>();
 
@@ -96,10 +96,73 @@ namespace WebServices
                             result_.Message = "Üyelik Bilgileri Güncellendi!";
                             result_.Success = true;
                         }
+                        else if (userInfo_ == null && !IsNewUser_)
+                        {
+                            result_.Message = "Girilen Bilgilerde Bir Kullanıcı Bulunamadı!";
+                            result_.Success = false;
+                        }
                         else
                         {
                             result_.Message = "Girilen Kullanıcı Adı ve ya Mail Adresi Sistemde Tanımlı!";
                             result_.Success = false;
+                        }
+                    }
+                }
+            }
+            catch (Exception Ex_)
+            {
+                result_.Message = Ex_.GetBaseException().ToString();
+                result_.Success = false;
+            }
+
+            return result_;
+        }
+
+        [WebMethod]
+        public Result<bool> DoInsertAndUpdateAdvert(bool IsNewAdvert_, int AdvertID_)
+        {
+            Result<bool> result_ = new Result<bool>();
+
+            try
+            {
+                using (GRADUATIONEntities ent_ = new GRADUATIONEntities())
+                {
+                    using (TransactionScope scope_ = new TransactionScope())
+                    {
+                        if (IsNewAdvert_)
+                        {
+                            ADVERT advert_ = new ADVERT();
+
+
+
+                            ent_.AddToADVERT(advert_);
+                            ent_.SaveChanges();
+                            scope_.Complete();
+                            ent_.AcceptAllChanges();
+
+                            result_.Message = "İlan Başarıyla Kaydedildi!";
+                            result_.Success = true;
+                        }
+                        else
+                        {
+                            ADVERT advertInfo_ = ent_.ADVERT.Where(x => x.ADVT_ID == (AdvertID_)).FirstOrDefault();
+                            if (advertInfo_ != null)
+                            {
+
+
+
+                                ent_.SaveChanges();
+                                scope_.Complete();
+                                ent_.AcceptAllChanges();
+
+                                result_.Message = "İlan Bilgileri Güncellendi!";
+                                result_.Success = true;
+                            }
+                            else
+                            {
+                                result_.Message = "İlan Bilgileri Güncellenemedi!";
+                                result_.Success = false;
+                            }
                         }
                     }
                 }
